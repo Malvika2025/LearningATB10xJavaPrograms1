89917ba90fc23afa05e8e75629e8dc76f37f3a77
Malvika2025 <malabika.ena@gmail.com>
2025-01-28 12:44:38 -0800

    2nd Commit

--- src/ex_01_Java_Basic/Lab008_Comments.java
+++ src/ex_01_Java_Basic/Lab008_Comments.java
@@ -1,4 +1,10 @@
 package ex_01_Java_Basic;
 
 public class Lab008_Comments {
+    /**
+     * Author -Malvika
+     * Course -ATB10x
+     * @param args
+     */
+    public static void main(String[] args){}
 }
