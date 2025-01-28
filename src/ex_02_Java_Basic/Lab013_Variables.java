3850e19e325dd15768dfa2795ffb1b5e24392917
Malvika2025 <malabika.ena@gmail.com>
2025-01-28 12:34:43 -0800

    2nd Commit

---
+++ src/ex_02_Java_Basic/Lab013_Variables.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basic;
+
+public class Lab013_Variables {
+    public static void main(String[] args) {
+        int a=10;
+        int age=65;
+        age=89;
+        System.out.println(age);
+        age=age+1;
+        System.out.println(age);
+    }
+}
